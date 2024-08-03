2a04a137a8ffa050ad8adfa08ddbf6ede8d1a954
davysongs <davidgsongs@gmail.com>
2024-08-03 15:34:06 +0100

    "Added Flask-Migrate to app/__init__.py and updated requirements.txt"

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  
 
 from .config import Config
 
 db = SQLAlchemy()
+migrate = Migrate() 
 login_manager = LoginManager()
 ckeditor = CKEditor()
 gravatar = Gravatar(size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
@@ -17,6 +19,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     ckeditor.init_app(app)
     Bootstrap5(app)
